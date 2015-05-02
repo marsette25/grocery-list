ff4c7473bd6a02770f923ae455251ed356a5434c
marsette25 <marsie.thomas@gmail.com>
2015-05-02 09:13:42 -0500

    changes

--- todo/models/todo.js
+++ todo/models/todo.js
@@ -4,9 +4,11 @@ var todoSchema = mongoose.Schema({
     due_date: {type: Date, required: true, default: Date.now },
     description: {type: String, required: true, default: ''},
     title: {type: String, required: true, default: ''},
-    priority: {type: Number, required: true, default: 1 },
+    priority: {type: String, required: true, default: 1 },
     complete: {type: Boolean, required: true, default: false },
-    user: {type: String, required: true}
+    user: {type: String, required: true},
+    category: {type: String, required: true, default: false },
+
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
