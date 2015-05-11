d1e13539f43ab449fad2f54c857f403e0f729d0b
marsette25 <marsie.thomas@gmail.com>
2015-05-11 18:26:17 -0500

    trying heroku

--- todo/db.js
+++ todo/db.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://localhost/test');
+
+mongoose.connect(process.env.DB);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
